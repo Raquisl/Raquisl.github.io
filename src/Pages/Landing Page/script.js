54bdca641572754c1261576ba50634b1977432c8
Raqui.sl <raqueltavaressantos@icloud.com>
2023-05-13 17:12:17 +0100

    More Java and some deets

--- src/Pages/Landing Page/script.js
+++ src/Pages/Landing Page/script.js
@@ -22,5 +22,20 @@
         window.location.href = `${window.location.origin}/src/Pages/Intro%20Page/index.html`;
       }
 
+      window.onscroll = function() {
+        var targetSection = document.getElementById("openingSectionScroll");
+        var targetPosition = targetSection.offsetTop;
+        var currentPosition = window.pageYOffset;
+        var header = document.getElementById("header")
+       
+        if(currentPosition >= targetPosition){
+          header.classList.add('pinkheader')
+        }   
+        else{
+          header.classList.remove('pinkheader')
+        }
+       
+      }
+
 
 
